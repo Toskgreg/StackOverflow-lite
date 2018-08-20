0115983d4c58ae9f089581f792f210ab7615cfc5
Toskgreg <toskingregory@icloud.com>
2018-08-20 15:46:40 +0300

    adding the first testdata python file.

---
+++ test_data.py
@@ -0,0 +1,18 @@
+post_question1 = {
+    "question_title": "What is an API ?",
+    "question_description": "Am new to python and recently came across the term API can any body help me describe in detail what an API is?"
+
+}
+
+
+post_answer = {
+    "text": "Nairobi",
+}
+post_comment = {
+    "text1": "Nairobi",
+}
+
+duplicate_question = {
+    "question_title": "What is an API ?",
+    "question_description": "Am new to python and recently came across the term API can any body help me describe in detail what an API is?"
+}
