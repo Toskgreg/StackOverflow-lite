2ddf4db48f352fb8b0cab2da4bfd4612f704119b
Toskgreg <toskingregory@icloud.com>
2018-08-20 16:18:57 +0300

    [#159849216]adding get_question_by ID function and app route

--- app/question/views.py
+++ app/question/views.py
@@ -6,3 +6,6 @@ QUESTION_APP = Blueprint('QUESTION_APP', __name__)
 QUESTION_VIEW = QUESTIONAPI.as_view('question_api')
 QUESTION_APP.add_url_rule('/api/v1/questions/', defaults={'question_id': None},
                       view_func=QUESTION_VIEW, methods=['GET', ])
+
+QUESTION_APP.add_url_rule('/api/v1/questions/<question_id>', view_func=QUESTION_VIEW,
+                      methods=['GET', ])
